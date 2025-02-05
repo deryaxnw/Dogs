7f9b5d1f20221b223a114360ffcb42a72fba3dda
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-05 18:16:29 -0300

    useContext provendo para meu Useform

--- src/Hooks/UserContext.jsx
+++ src/Hooks/UserContext.jsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { TOKEN_POST, USER_GET } from "../../service/instace";
 
 
 
@@ -6,10 +7,33 @@ import { createContext } from "react";
 export const UserContext = createContext();
 
 export const UserStorage = ({ children }) => {
-  
+  const [data, setData] = useState(null)
+  const [login, setLogin] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [erro, setError] = useState(null)
+
+
+async function getUser(token) {
+    const {url, options} = USER_GET(token)
+    const response = await fetch(url, options);
+    const json = await response.json()
+    setData(json);
+    setLogin(true)
+    console.log(json);
+    
+}
+
+
+  async function userLogin(username, password) {
+    const {url, options} = TOKEN_POST({username, password})
+    const tokenRes = await fetch(url, options);
+    const {token} = await tokenRes.json();
+    window.localStorage.setItem('token', token)
+    getUser(token)
+  }
 
     return (
-        <UserContext.Provider>
+        <UserContext.Provider value={{data, userLogin}}>
             {children}
         </UserContext.Provider>
     );
