a069972c4f23521c01c26228c18145e02cedc182
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-06 16:17:39 -0300

    useNavigate para o usuario logado nao ter acesso ao form de login, Uso do useEffect para evitar looping de rota

--- src/Hooks/UserContext.jsx
+++ src/Hooks/UserContext.jsx
@@ -1,9 +1,10 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import {
   TOKEN_POST,
   TOKEN_VALIDATE_POST,
   USER_GET,
 } from "../../service/instace";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
@@ -12,28 +13,17 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = useState(null);
   const [loading, setLoading] = useState(false);
   const [erro, setError] = useState(null);
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    async function autoLogin() {
-      const token = window.localStorage.getItem("token");
-      if (token) {
-        try {
-          setError(null)
-          setLoading(true)
-          const { url, options } = TOKEN_VALIDATE_POST(token);
-          const response = await fetch(url, options);
-          if (!response.ok) throw new Error("TOKEN INVÁLIDO");
+  const userLogout = useCallback( async function userLogout() {
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem("token");
+    navigate('/login')
+  }, [navigate])
 
-          const json = await response.json();
-          await getUser(token)
-        } catch (erro) {
-        } finally {
-          setLoading(false)
-        }
-      }
-    }
-    autoLogin();
-  }, []);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -45,23 +35,48 @@ export const UserStorage = ({ children }) => {
   }
 
   async function userLogin(username, password) {
-    const { url, options } = TOKEN_POST({ username, password });
-    const tokenRes = await fetch(url, options);
-    const { token } = await tokenRes.json();
-    window.localStorage.setItem("token", token);
-    getUser(token);
+    try {
+      setError(null)
+      setLoading(true)
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenRes = await fetch(url, options);
+      if(!tokenRes.ok) throw new Error(`Error: Usuário inválido`);
+      
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem("token", token);
+      await getUser(token);
+      setLogin(true)
+    } catch (erro) {
+      setError(erro.message);
+      setLogin(false);
+    } finally {
+      setLoading(false)
+    }
   }
 
-  async function userLogout(){
-    setData(null)
-    setError(null)
-    setLoading(false)
-    setLogin(false)
-    window.localStorage.removeItem('token')
-  }
-
+  useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem("token");
+      if (token && login === null) { // Apenas executa se login for null
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error("TOKEN INVÁLIDO");
+  
+          await getUser(token);
+        } catch (erro) {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, [userLogout, login]); // Agora depende de login para evitar loops
   return (
-    <UserContext.Provider value={{ data, userLogin , userLogout}}>
+    <UserContext.Provider value={{ data, userLogin, userLogout , loading, login, erro}}>
       {children}
     </UserContext.Provider>
   );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { LoginForm } from "./LoginForm";
 import { LoginCreate } from "./LoginCreate";
 import { LoginPasswordLost } from "./LoginPasswordLost";
 import { LoginPasswordReset } from "./LoginPasswordReset";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../../Hooks/UserContext";
 
 export const Login = () => {
-    return ( 
-       <>
-        <Routes>
-            <Route path="/" element={<LoginForm/>}/>
-            <Route path="criar" element={<LoginCreate/>}/>
-            <Route path="perdeu" element={<LoginPasswordLost/>}/>
-            <Route path="resetar" element={<LoginPasswordReset/>}/>
-        </Routes>
-       
-       </>
-    );
-}
+  const { login } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (login === true) {
+      navigate("/conta", { replace: true }); // replace evita múltiplos push na stack do navegador
+    }
+  }, [login, navigate]);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </>
+  );
+};
