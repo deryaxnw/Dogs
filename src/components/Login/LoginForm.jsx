5d4588d32bb7a8c849ff4bff35c8dd6e489d6269
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-06 16:16:24 -0300

    descriçao de ação para o botao e mensagem de erro

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -10,16 +10,15 @@ export const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const {userLogin} = useContext(UserContext)
+  const { userLogin, erro, loading } = useContext(UserContext);
 
-  
   async function handleSubmit(e) {
     e.preventDefault();
 
     if (username.validate() && password.validate()) {
-      userLogin(username.value, password.value)
+      userLogin(username.value, password.value);
+    }
   }
-}
 
   return (
     <section>
@@ -27,7 +26,12 @@ export const LoginForm = () => {
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Entrar</Button>
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {erro && <p>{erro}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
