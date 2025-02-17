ecccfbd2d4da1780e403b119bff686a42c998b7b
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-17 14:51:34 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { UserStorage } from "./Hooks/UserContext";
+import { Protected } from "./components/Header/ProtectedRouter/Protected";
+import { User } from "./components/User/User";
+
 import "./App.css";
-
-
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <UserStorage>
-          <Header/>
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <Protected>
+                  <User />
+                </Protected>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
