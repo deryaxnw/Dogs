78e54adc8bae8b674956307d079b04504ba5d46f
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-03-30 12:30:03 -0300

    feat: feed and organization photos style

--- src/components/Feed/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos.jsx
@@ -3,30 +3,31 @@ import { UseFetch } from "../../Hooks/UseFetch";
 import { FeedPhotosItem } from "./FeedPhotosItem";
 import { PHOTOS_GET } from "../../../service/instace";
 import { Error } from "../Help/Error";
-
+import { Loading } from "../Help/Loading";
+import styles from "./FeedPhotos.module.css";
 
 export const FeedPhotos = () => {
 
-    const {data , loading , error, request} = UseFetch()
-
+    const { data, loading, error, request } = UseFetch()
     useEffect(() => {
         async function FetchPhotos() {
-            const {url, options} = PHOTOS_GET({page: 1, total: 6, user:0})
-           const {response , json} = await request(url, options)
-           console.log(json);
-           
+            const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 })
+            const { response, json } = await request(url, options)
+            console.log(json);
+
         }
         FetchPhotos()
-    } ,[request])
+    }, [request])
 
 
-    if (error) return <Error error={error}/>
-    if(loading) return <Loading />
-    return ( <>
-    
-    <div>
-        <FeedPhotosItem />
-    </div>
-    
-    </> );
+    if (error) return <Error error={error} />
+    if (loading) return <Loading />
+    if (data)
+        return (
+            <ul className={`${styles.feed} animeLeft`}>
+                {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo} />)}
+            </ul>
+        );
+
+    else return null
 }
--- src/components/Feed/FeedPhotosItem.jsx
+++ src/components/Feed/FeedPhotosItem.jsx
@@ -1,9 +1,17 @@
-export const FeedPhotosItem = () => {
-    return ( <>
-    
-    <div>
 
-    </div>
+import styles from "./FeedPhotosItem.module.css";
+
+export const FeedPhotosItem = ({ photo }) => {
+
     
-    </> );
-}
+    return(
+        <li className={styles.photo}>
+        <img src={photo.src} alt={photo.title} />
+        <span>{photo.acessos}</span>
+    </li>
+
+   
+    
+    )
+    
+};
